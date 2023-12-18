1b8e36cac9f05b9cca0400e2f12ce61a40fd4218
Jonathan Alvarado-Gomez <jonathan.alvaradogomez@revature.netgit config --global user.email jonathan.alvaradogomez@revature.netgit config --global user.email alvarado.jntn@gmail.comgit config --global user.name Jonathan>
2023-12-18 10:58:06 -0800

    Adding @GetMapping

--- src/main/java/com/alvarado/movies/MoviesApplication.java
+++ src/main/java/com/alvarado/movies/MoviesApplication.java
@@ -2,14 +2,23 @@ package com.alvarado.movies;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-// we are now on minute 32 of the video.
+
+@RestController
+
 
 public class MoviesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoviesApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String apiRoot(){
+		return "Hello World!";
 	}
 
 }
