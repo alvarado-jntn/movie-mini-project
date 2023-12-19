f8ab82819f2d846d1df1cf35c401f529d84fb147
Jonathan Alvarado-Gomez <jonathan.alvaradogomez@revature.netgit config --global user.email jonathan.alvaradogomez@revature.netgit config --global user.email alvarado.jntn@gmail.comgit config --global user.name Jonathan>
2023-12-19 13:31:39 -0800

    MovieController updated to have Autowire and GetMappings

--- src/main/java/com/alvarado/movies/Services/MovieService.java
+++ src/main/java/com/alvarado/movies/Services/MovieService.java
@@ -2,16 +2,23 @@ package com.alvarado.movies.Services;
 
 import com.alvarado.movies.Movie;
 import com.alvarado.movies.Repositories.MovieRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
     @Autowired
     private MovieRepository movieRepository;
-    public List<Movie> allMovies(){
-        return movieRepository.findAll() ; //This is described inside the Mongo repository class.
+
+    public List<Movie> allMovies() {
+        return movieRepository.findAll(); //This is described inside the Mongo repository class.
+    }
+
+    public Optional<Movie> singleMovie(ObjectId id) {
+        return movieRepository.findById(id);
     }
 }
