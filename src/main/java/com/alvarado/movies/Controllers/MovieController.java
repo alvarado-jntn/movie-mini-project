fd1481f25981237ca2f2b850e47b14f52cc07706
Jonathan Alvarado-Gomez <jonathan.alvaradogomez@revature.netgit config --global user.email jonathan.alvaradogomez@revature.netgit config --global user.email alvarado.jntn@gmail.comgit config --global user.name Jonathan>
2023-12-19 11:45:01 -0800

    Updated MovieController class. Updated MovieService class.

--- src/main/java/com/alvarado/movies/Controllers/MovieController.java
+++ src/main/java/com/alvarado/movies/Controllers/MovieController.java
@@ -1,5 +1,8 @@
 package com.alvarado.movies.Controllers;
 
+import com.alvarado.movies.Movie;
+import com.alvarado.movies.Services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,11 +10,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/movies")
 public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
     @GetMapping
-    public ResponseEntity<String> allMovies(){
-        return new ResponseEntity<String>("All Movies!", HttpStatus.OK);
+    public ResponseEntity<List<Movie>> getAllMovies() {
+        return new ResponseEntity<List<Movie>>(movieService.allMovies(), HttpStatus.OK);
     }
 }
--- src/main/java/com/alvarado/movies/Services/MovieService.java
+++ src/main/java/com/alvarado/movies/Services/MovieService.java
@@ -1,7 +1,17 @@
 package com.alvarado.movies.Services;
 
+import com.alvarado.movies.Movie;
+import com.alvarado.movies.Repositories.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    public List<Movie> allMovies(){
+        return movieRepository.findAll() ; //This is described inside the Mongo repository class.
+    }
 }
