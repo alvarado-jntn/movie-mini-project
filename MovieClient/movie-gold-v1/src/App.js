44220eb09ca46adbbd1c66c29e9617623ef77543
Jonathan Alvarado-Gomez <jonathan.alvaradogomez@revature.netgit config --global user.email jonathan.alvaradogomez@revature.netgit config --global user.email alvarado.jntn@gmail.comgit config --global user.name Jonathan>
2023-12-21 20:56:44 -0800

    Many implementations on front end

--- MovieClient/movie-gold-v1/src/App.js
+++ MovieClient/movie-gold-v1/src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
 
 
 function App() {
 
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
 
   const getMovies = async () => {
 
     try {
       const response = await api.get("/api/v1/movies");
-      console.log(response.data);
       setMovies(response.data);
 
     } catch (err) {
@@ -27,6 +30,12 @@ function App() {
 
   return (
     <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home movies={movies} />}></Route>
+        </Route>
+      </Routes>
 
     </div>
   );
