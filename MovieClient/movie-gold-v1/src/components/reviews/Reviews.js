39bc01f3a046c5b81d2f1ce45695c43e25f34138
Jonathan Alvarado-Gomez <jonathan.alvaradogomez@revature.netgit config --global user.email jonathan.alvaradogomez@revature.netgit config --global user.email alvarado.jntn@gmail.comgit config --global user.name Jonathan>
2023-12-26 17:27:50 -0800

    Review component needs more work

---
+++ MovieClient/movie-gold-v1/src/components/reviews/Reviews.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { useEffect, useRef } from 'react';
+import api from '../../api/axiosConfig';
+import { useParams } from 'react-router-dom';
+import { Container, Row, Col } from '../reviewForm/ReviewForm';
+
+const Reviews = ({ getMovieData, movie, reviews, setReviews }) => {
+
+    const revText = useRef();
+    let params = useParams();
+    const movieId = params.movieId;
+
+    useEffect(() => {
+        getMovieData(movieId);
+    }, [])
+    return (
+        <Container>
+            <Row>
+                <Col><h3>Reviews</h3></Col>
+            </Row>
+            <Row className="mt-2">
+                <Col>
+                    <img src={movie?.poster} alt="" />
+                </Col>
+                <Col>
+                    {
+                        <>
+                            <Row>
+                                <Col></Col>
+                            </Row>
+                        </>
+                    }
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Reviews;
